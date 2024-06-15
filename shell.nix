99f433fee71533aad4df9136fdc81c33be2457f9
danstarns <danielstarns@hotmail.com>
2024-06-15 13:45:35 +0100

    fix: add path back

--- shell.nix
+++ shell.nix
@@ -13,8 +13,7 @@ pkgs.mkShell {
     # workaround https://github.com/sbt/sbt-assembly/issues/496
     export LC_ALL=C.UTF-8
     
-    export PATH="$HOME/go/bin:$HOME/.cargo/bin"
-
+    export PATH="$PATH:$HOME/go/bin:$HOME/.cargo/bin"
     go install github.com/codesenberg/bombardier@latest
 
     cargo install --git https://github.com/tailcallhq/tailcall
